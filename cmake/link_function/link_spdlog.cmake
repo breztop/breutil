3e058f9be75c486dd51b34b8ed038452a6fe43e6
breeze <2329484200@qq.com>
2026-06-10 22:43:39 +0800

    feat: :sparkles: nljhmann json 和 spdlog 的link函数

--- cmake/link_function/link_spdlog.cmake
+++ cmake/link_function/link_spdlog.cmake
@@ -1,6 +1,23 @@
-# spdlog 链接函数
-set(SPDLOG_DIR_PATH "${CMAKE_SOURCE_DIR}/thirdLib/spdlog")
 function(link_spdlog target)
-    # 设置 spdlog 的包含目录
-    include_directories(${SPDLOG_DIR_PATH}/include)
+    if(NOT TARGET spdlog::spdlog)
+        find_package(spdlog QUIET)
+        if(NOT spdlog_FOUND)
+            include(FetchContent)
+
+            FetchContent_Declare(
+                spdlog
+                GIT_REPOSITORY https://github.com/gabime/spdlog.git
+                GIT_TAG        v1.17.0  # 请根据需要调整版本
+            )
+            set(SPDLOG_USE_STD_FORMAT OFF CACHE BOOL "Use std::format for spdlog" FORCE)
+
+            FetchContent_MakeAvailable(spdlog)
+
+            message(STATUS "spdlog 已下载并添加到项目中")
+        endif()
+    endif()
+
+    target_link_libraries(${target} PUBLIC spdlog::spdlog)
+
+    target_precompile_headers(${target} PRIVATE <spdlog/spdlog.h>)
 endfunction()
